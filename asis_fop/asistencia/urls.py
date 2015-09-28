058730c4068bd9994cdbd978e7d4b0b6aa2c262a
Rene Rios <rene@minggalabs.com>
2015-09-28 17:40:50 -0300

    ASIS: navega hasta lista

--- asis_fop/asistencia/urls.py
+++ asis_fop/asistencia/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^colegio/(?P<colegio_id>[0-9]+)/$', views.cursos, name='cursos'),
+    url(r'^curso/(?P<curso_id>[0-9]+)/$', views.lista, name='lista'),
 ]
--- asis_fop/asistencia/views.py
+++ asis_fop/asistencia/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import *
 
 
 def index(request):
-    return HttpResponse("Funcionando app de asistencia")
+	context = Colegio.objects.all()
+	return render(request, 'asistencia/index.html', {'lista': context, 'tipo':'colegios'})
+
+def cursos(request, colegio_id):
+	colegio = Colegio.objects.get(pk=colegio_id)
+	context = Curso.objects.filter(colegio=colegio)
+	return render(request, 'asistencia/index.html', {'lista': context, 'tipo':'cursos'})
+
+def lista(request, curso_id):
+	curso = Curso.objects.get(pk=curso_id)
+	listacurso = ListaCurso.objects.filter(curso=curso, periodo='2015')
+	context = Alumno.objects.filter(listacurso=listacurso)
+	return render(request, 'asistencia/index.html', {'lista': context, 'tipo':'alumnos'})
