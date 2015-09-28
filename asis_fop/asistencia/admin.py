908edeeb0755e529570254e8a153824770cc3ae1
rriosmingga <rene@minggalabs.com>
2015-09-27 22:53:23 -0300

    ASIS: url

--- asis_fop/asistencia/admin.py
+++ asis_fop/asistencia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Colegio)
+admin.site.register(Curso)
+admin.site.register(Alumno)
+admin.site.register(ListaCurso)
+admin.site.register(Profesor)
+admin.site.register(ProfesorJefe)
